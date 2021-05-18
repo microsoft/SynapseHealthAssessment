f6e727f5924ee61b9a202e1571c70a62ddf495fb
Nick Salch <nick.salch@outlook.com>
2021-05-18 13:16:51 -0700

    organizing

--- SQL_Scripts/CCIDeletedRows.sql
+++
@@ -1,9 +0,0 @@
-﻿SELECT  [Schema_Name],
-		[Table_Name],
-		Distribution_Type,
-		SUM(rg_total_rows) AS compressed_total_rows,
-		SUM(rg_deleted_rows) AS compressed_deleted_rows
-	FROM [dbo].[CCI_MetaData]
-	WHERE rg_state = 3
-	GROUP BY [Schema_Name], [Table_Name], Distribution_Type
-	ORDER BY [Schema_Name], [Table_Name]
