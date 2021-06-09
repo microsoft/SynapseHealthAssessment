c39f8a2a2af2beedb727ad85c27264910a8216a4
Nick Salch <nick.salch@outlook.com>
2021-06-09 13:46:32 -0700

    Update FindMismatchedDataTypes.sql

--- SQL_Scripts/TableHealth/FindMismatchedDataTypes.sql
+++ SQL_Scripts/TableHealth/FindMismatchedDataTypes.sql
@@ -1,9 +1,14 @@
 ﻿--Identify columns that have the same name, but different data types or data lengths
 WITH Query as 
 (
-SELECT COLUMN_NAME, COUNT(DISTINCT DATA_TYPE)AS 'NumDiffTypes' ,COUNT(DISTINCT Character_Maximum_length) AS 'NumDiffLengths'
+SELECT COLUMN_NAME, COUNT(DISTINCT DATA_TYPE)AS 'Type_Count' ,COUNT(DISTINCT Character_Maximum_length) AS 'Length_Count', STRING_AGG(Table_Name, ', ') AS Found_In_Tables
 FROM INFORMATION_SCHEMA.COLUMNS
 GROUP BY COLUMN_NAME
 )
-SELECT * FROM Query WHERE NumDiffTypes > 1 or NumDiffLengths > 1
-Order by NumDiffTypes desc,NumDiffLengths desc
+SELECT * FROM Query WHERE Type_Count > 1 or Length_Count > 1
+Order by Type_Count desc,Length_Count desc
+
+--Use this query to see all individual uses of a specific column name including data type and length
+SELECT TABLE_SCHEMA,TABLE_NAME, COLUMN_NAME, DATA_TYPE, Character_Maximum_length
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME = 'DateID' --Change to column name to investigate
