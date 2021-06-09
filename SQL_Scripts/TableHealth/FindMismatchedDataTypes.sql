a7df790b435befbe1b59549b96237a3af701c4db
Nick Salch <nick.salch@outlook.com>
2021-06-09 13:32:52 -0700

    Create FindMismatchedDataTypes.sql

---
+++ SQL_Scripts/TableHealth/FindMismatchedDataTypes.sql
@@ -0,0 +1,9 @@
+﻿--Identify columns that have the same name, but different data types or data lengths
+WITH Query as 
+(
+SELECT COLUMN_NAME, COUNT(DISTINCT DATA_TYPE)AS 'NumDiffTypes' ,COUNT(DISTINCT Character_Maximum_length) AS 'NumDiffLengths'
+FROM INFORMATION_SCHEMA.COLUMNS
+GROUP BY COLUMN_NAME
+)
+SELECT * FROM Query WHERE NumDiffTypes > 1 or NumDiffLengths > 1
+Order by NumDiffTypes desc,NumDiffLengths desc
