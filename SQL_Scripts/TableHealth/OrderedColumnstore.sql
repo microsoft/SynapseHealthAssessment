719a2d23f7f1a7dbc1271041b51b9aa96111c35e
Nick Salch <nick.salch@outlook.com>
2021-06-09 14:06:06 -0700

    Create OrderedColumnstore.sql

---
+++ SQL_Scripts/TableHealth/OrderedColumnstore.sql
@@ -0,0 +1,24 @@
+﻿--Check for tables that were created with ordered columnstore
+SELECT object_name(c.object_id) table_name, c.name column_name, i.column_store_order_ordinal 
+FROM sys.index_columns i 
+JOIN sys.columns c ON i.object_id = c.object_id AND c.column_id = i.column_id
+WHERE column_store_order_ordinal <>0;
+
+
+--Check Segment Overlap
+SELECT o.name, pnp.index_id, 
+cls.row_count, pnp.data_compression_desc, 
+pnp.pdw_node_id, pnp.distribution_id, cls.segment_id, 
+cls.column_id, 
+cls.min_data_id, cls.max_data_id, 
+cls.max_data_id-cls.min_data_id as difference
+FROM sys.pdw_nodes_partitions AS pnp
+   JOIN sys.pdw_nodes_tables AS Ntables ON pnp.object_id = NTables.object_id AND pnp.pdw_node_id = NTables.pdw_node_id
+   JOIN sys.pdw_table_mappings AS Tmap  ON NTables.name = TMap.physical_name AND substring(TMap.physical_name,40, 10) = pnp.distribution_id
+   JOIN sys.objects AS o ON TMap.object_id = o.object_id
+   JOIN sys.pdw_nodes_column_store_segments AS cls ON pnp.partition_id = cls.partition_id AND pnp.distribution_id  = cls.distribution_id
+JOIN sys.columns as cols ON o.object_id = cols.object_id AND cls.column_id = cols.column_id
+WHERE o.name = '<table_name>' and cols.name = '<column_name>'  and TMap.physical_name  not like '%HdTable%' --probably need to join to sys.schemas as well
+ORDER BY o.name, pnp.distribution_id, cls.min_data_id;
+
+
