bbe727e1da6a075cdd85e7e8c6ab5ece61a94442
Nick Salch <nick.salch@outlook.com>
2021-06-09 17:56:40 -0700

    Create tablesByDistributionType.sql

---
+++ SQL_Scripts/TableHealth/tablesByDistributionType.sql
@@ -0,0 +1,12 @@
+﻿--Table size by distribution type
+--PRE-REQUISITE: must have table info view created already
+     distribution_policy_name
+,   count(1)                         as table_count
+,    SUM(row_count)                as table_type_row_count
+,    SUM(reserved_space_GB)        as table_type_reserved_space_GB
+,    SUM(data_space_GB)            as table_type_data_space_GB
+,    SUM(index_space_GB)           as table_type_index_space_GB
+,    SUM(unused_space_GB)          as table_type_unused_space_GB
+FROM dbo.vTableSizes
+GROUP BY distribution_policy_name
+;
